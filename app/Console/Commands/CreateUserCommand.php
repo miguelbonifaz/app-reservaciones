0c474ea12f1c600ef5aeccd44c48bb7c5a26d293
miguelbonifaz <miguelbonifaz126@gmail.com>
2021-07-04 18:36:58 -0500

    Added Laravel Breeze, New Command to create a test user.

---
+++ app/Console/Commands/CreateUserCommand.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class CreateUserCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'create:user';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a user';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        User::updateOrCreate([
+            'name' => 'Miguel Bonifaz',
+            'email' => 'booking@gmail.com',
+        ], ['password' => bcrypt('booking')]);
+    }
+}
