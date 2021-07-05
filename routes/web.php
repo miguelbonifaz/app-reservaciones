aab002161ebfac06643699a1352718cefe0ad6de
miguelbonifaz <miguelbonifaz126@gmail.com>
2021-07-04 19:05:27 -0500

    removed view welcome and added redirection from "/" to "/login".

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->to(
+        route('login')
+    );
 });
 
 Route::get('/dashboard', function () {
