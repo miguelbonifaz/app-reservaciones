e920bad0f900a3264557cc6bd8cf117c0b7d1547
miguelbonifaz <miguelbonifaz126@gmail.com>
2021-07-08 21:47:57 -0500

    updated booking-app.yml file.

--- tests/Feature/Users/ListUsersTest.php
+++ tests/Feature/Users/ListUsersTest.php
@@ -1,18 +1,19 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 uses(RefreshDatabase::class);
 
-test('has users', function () {
-    
-    // Arrange  
-    $user = \App\Models\User::factory()->create();
+test('can see user list', function () {
+    // Arrange
+    $user = User::factory()->create();
+
+    $url = route('users.index');
 
     // Act
-    $url = route('users.index');
-    
-    $response = $this->actingAs($user)->get($url);    
+    $response = $this->actingAs($user)->get($url);
+
     // Assert
     $response->assertOk();
 });
