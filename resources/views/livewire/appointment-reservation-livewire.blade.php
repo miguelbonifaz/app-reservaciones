c5e46af1847b9ce75ef3560438a4bdd8f56aab37
miguelbonifaz <miguelbonifaz126@gmail.com>
2021-07-31 13:57:10 -0500

    Ahora se puede escojer un empleado y un servicio.

--- resources/views/livewire/appointment-reservation-livewire.blade.php
+++ resources/views/livewire/appointment-reservation-livewire.blade.php
@@ -3,6 +3,33 @@
         use App\Http\Livewire\AppointmentReservationLivewire;
     @endphp
     <div class="mx-auto max-w-5xl sm:rounded-md">
+        <div class="hidden grid-cols-5 gap-2 mb-6 md:grid">
+            <div>
+                <label class="text-sm font-bold text-mariajose_gray" for="">1.Servicio</label>
+                <div class="h-3 rounded-l-md bg-mariajose_gray {{ $this->firstStepProgressBarClass }}">
+                </div>
+            </div>
+            <div>
+                <label class="text-sm font-bold text-mariajose_gray" for="">2.Hora</label>
+                <div class="h-3 bg-mariajose_gray {{ $this->secondStepProgressBarClass }}">
+                </div>
+            </div>
+            <div>
+                <label class="text-sm font-bold text-mariajose_gray" for="">3.Información</label>
+                <div class="h-3 bg-mariajose_gray {{ $this->thirdStepProgressBarClass }}">
+                </div>
+            </div>
+            <div>
+                <label class="text-sm font-bold text-mariajose_gray" for="">4.Detalles</label>
+                <div class="h-3 bg-mariajose_gray {{ $this->fourthStepProgressBarClass }}">
+                </div>
+            </div>
+            <div>
+                <label class="text-sm font-bold text-mariajose_gray" for="">5.Reservación</label>
+                <div class="h-3 rounded-r-md bg-mariajose_gray {{ $this->fifthStepProgressBarClass }}">
+                </div>
+            </div>
+        </div>
         <div class="bg-white">
             <div class="grid gap-6 sm:grid-cols-2">
                 @if ($currentStep == AppointmentReservationLivewire::STEP_SERVICE_AND_EMPLOYEE)
